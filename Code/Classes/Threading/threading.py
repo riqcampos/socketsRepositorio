d22e6a550ee942c0964b9025a8bfcbe42c571140
César "kaiser" Campos <cesarhrccontato@gmail.com>
2025-02-27 09:43:13 -0300

    setting up directory interface

--- Code/Classes/Threading/threading.py
+++
@@ -1,7 +0,0 @@
-import threading
-
-class threadParalelismSystem:
-    def __init__(self, numThreads):
-        self.numThreads = numThreads
-        self.threads = []
-        self.threadLock = threading.Lock
---
+++ Code/Main/man.py
@@ -0,0 +1,3 @@
+from client.py import Client
+from server.py import Server
+from threading.py import threadParalelismSystem
